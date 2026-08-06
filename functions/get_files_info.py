10110ff0706fed0a36d4de28cd9e074f5e5cf5b6
luiki-dev <przemyslaw.lewicki.dev@proton.me>
2026-08-06 15:59:51 +0200

    Add directory contents listing

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -4,23 +4,36 @@ from pathlib import Path
 def get_files_info(working_directory: str, directory: str = ".") -> str:
     try:
         absolute_path_working_directory = Path(working_directory).resolve()
-        target_directory_path = (
-            absolute_path_working_directory / directory
-        ).resolve()
+        directory_path = (absolute_path_working_directory / directory).resolve()
 
         # check wether directory is in working directory
         target_directory_valid = (
-            target_directory_path == absolute_path_working_directory
-            or absolute_path_working_directory in target_directory_path.parents
+            directory_path == absolute_path_working_directory
+            or absolute_path_working_directory in directory_path.parents
         )
 
         if not target_directory_valid:
-            return f'Error: Cannot list "{directory}"'
-            "as it is outside the permitted working directory"
+            return (
+                f'Error: Cannot list "{directory}" '
+                + "as it is outside the permitted working directory"
+            )
 
-        if not target_directory_path.is_dir():
+        if not directory_path.is_dir():
             return f'Error: "{directory}" is not a directory'
 
-        return f'Success: "{directory}" is within the working directory'
+        return get_dir_contents_info(directory_path)
     except Exception as e:  # noqa: BLE001
         return f"Error: {e}"
+
+
+def get_dir_contents_info(directory: Path) -> str:
+    directory_contents: str = f"Result for '{directory.name}' directory:\n"
+
+    for item in directory.iterdir():
+        directory_contents = directory_contents + (
+            f"- {item.name}: "
+            + f"file_size={item.stat(follow_symlinks=False).st_size} bytes, "
+            + f"is_dir={item.is_dir()}\n"
+        )
+
+    return directory_contents
--- test_get_files_info.py
+++ test_get_files_info.py
@@ -4,6 +4,6 @@ from functions.get_files_info import get_files_info
 # uv run python -m tests.test_get_files_info
 
 print(get_files_info("calculator", "."))
+print(get_files_info("calculator", "pkg"))
 print(get_files_info("calculator", "/bin"))
 print(get_files_info("calculator", "../"))
-print(get_files_info("calculator", "main.py"))
